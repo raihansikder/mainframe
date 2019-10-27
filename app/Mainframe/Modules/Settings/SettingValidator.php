1b014665659e92d42c2c5eb32c819660bcad76c5
Raihan S <raihan.act@gmail.com>
2019-10-28 01:19:05 +0600

    #

--- app/Mainframe/Modules/Settings/SettingValidator.php
+++ app/Mainframe/Modules/Settings/SettingValidator.php
@@ -17,7 +17,7 @@ class SettingValidator extends ModelValidator
     public static function rules($element, $merge = [])
     {
         $rules = [
-            'name' => 'required|between:1,255|unique:modules,name,'.(isset($element->id) ? "$element->id" : 'null').',id,deleted_at,NULL',
+            'name' => 'required|between:1,255|unique:modules,name,'.(isset($element->id) ? (string) $element->id : 'null').',id,deleted_at,NULL',
             'title' => 'required|between:1,255',
             'type' => 'required|'.'in:'.implode(',', array_keys(Setting::$types)),
             'desc' => 'between:1,2048',
@@ -42,13 +42,13 @@ class SettingValidator extends ModelValidator
 
         if ($setting->type === 'boolean') {
             if (! in_array($setting->value, ['true', 'false'])) {
-                $this->addError('value', "If boolean type is selected, value must be 'true' or 'false'");
+                $this->invalidate('value', "If boolean type is selected, value must be 'true' or 'false'");
             }
         }
 
         if ($setting->type === 'array') {
             if (! json_decode($setting->value)) {
-                $this->addError('value', "If array/json type is selected, value must be a valid json string");
+                $this->invalidate('value', "If array/json type is selected, value must be a valid json string");
             }
         }
 
