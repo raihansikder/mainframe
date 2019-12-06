69a9082fe8f1ca5ead709a2ebe725e22827bad96
Raihan S <raihan.act@gmail.com>
2019-12-06 18:20:30 +0600

    #

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -2,7 +2,6 @@
 
 include_once __DIR__."/helper-generic.php";
 include_once __DIR__."/helper-framework.php";
-include_once __DIR__."/helper-db-table.php";
 include_once __DIR__."/helper-tenant.php";
 include_once __DIR__."/helper-permission.php";
 include_once __DIR__."/helper-project.php";
