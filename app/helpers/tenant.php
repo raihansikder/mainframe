32ca5b664c769368e26d952b91096f5722c9fbec
Raihan Sikder <raihan.act@gmail.com>
2019-12-29 12:06:33 +0600

    #

--- app/helpers/tenant.php
+++ app/helpers/tenant.php
@@ -36,20 +36,7 @@ function tenantUser($user_id = null)
     return userTenantId($user_id);
 }
 
-/**
- * Returns the tenant id of the element.
- *
- * @param $element
- * @return bool
- */
-function elementTenantId($element)
-{
-    $tenant_idf = 'tenant_id';
-    if (isset($element->$tenant_idf) && $element->$tenant_idf) {
-        return $element->$tenant_idf;
-    }
-    return false;
-}
+
 
 /**
  * Checks if a module has tenant context.
@@ -79,50 +66,6 @@ function inTenantContext($moduleName, $user_id = null)
     return null;
 }
 
-/**
- * Checks if a specific element belongs to a the same tenant of the current user.
- *
- * @param      $element
- * @param null $user_id
- * @return bool
- */
-function elementBelongsToSameTenant($element, $user_id = null)
-{
-    $tenant_idf = 'tenant_id';
-    if (inTenantContext(moduleName(get_class($element))) && (userTenantId($user_id) == $element->$tenant_idf)) {
-        return true;
-    }
-    return false;
-}
-
-/**
- * During Model saving() we can inject facilityagency_id based on the logged in user's tenant_id
- *
- * @param Eloquent $model
- * @param null $user_id
- * @return \Eloquent
- */
-function fillTenantId(Eloquent $model, $user_id = null)
-{
-    $tenant_idf = 'tenant_id';
-    if (inTenantContext(moduleName(get_class($model)))) {
-        $model->$tenant_idf = userTenantId($user_id);
-    }
-    return $model;
-}
-
-/**
- * Checks if input has tenant_id value
- *
- * @return bool|mixed
- */
-function tenantInput()
-{
-    if (Request::has('tenant_id')) {
-        return Request::get('tenant_id');
-    }
-    return false;
-}
 
 /**
  * @param      $moduleName
@@ -136,20 +79,4 @@ function injectTenantIdInModelQuery($moduleName, $q, $user_id = null)
         $q = $q->where($moduleName . '.' . 'tenant_id', $tenant_id);
     }
     return $q;
-}
-
-/**
- * Injects tenant id match in SQL query
- *
- * @param      $moduleName
- * @param      $sql
- * @param null $user_id
- * @return string
- */
-function injectTenantIdInSqlQuery($moduleName, $sql, $user_id = null)
-{
-    if ($tenant_id = inTenantContext($moduleName, $user_id)) {
-        $sql = $sql . " AND " . dbTable($moduleName) . "." . 'tenant_id' . "= $tenant_id";
-    }
-    return $sql;
 }
