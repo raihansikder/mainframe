79bf87c90b1a30d97349aa3c1f920f6ac1ab6c1d
Raihan Sikder <raihan.act@gmail.com>
2020-01-19 10:48:08 +0600

    user datatable columns

--- app/Mainframe/Modules/Users/UserDatatable.php
+++ app/Mainframe/Modules/Users/UserDatatable.php
@@ -19,11 +19,31 @@ class UserDatatable extends ModuleDatatable
     {
         return [
             [$this->table.".id", 'id', 'ID'],
-            [$this->table.".name", 'name', 'Email'],
+            [$this->table.".email", 'email', 'Email'],
             ['updater.name', 'user_name', 'Updater'],
             [$this->table.".updated_at", 'updated_at', 'Updated at'],
             [$this->table.".is_active", 'is_active', 'Active']
         ];
+    }
+
+    /**
+     * Modify datatable values
+     *
+     * @return mixed
+     * @var $dt \Yajra\DataTables\DataTableAbstract
+     */
+    public function modify($dt)
+    {
+        // Set columns for HTML output.
+        $dt = $dt->rawColumns(['id', 'email', 'is_active']);
+
+        // Next modify each column content
+        /*  @var $dt \Yajra\DataTables\DataTableAbstract */
+        $dt = $dt->editColumn('email', '<a href="{{ route(\''.$this->module->name.'.edit\', $id) }}">{{$email}}</a>');
+        $dt = $dt->editColumn('id', '<a href="{{ route(\''.$this->module->name.'.edit\', $id) }}">{{$id}}</a>');
+        $dt = $dt->editColumn('is_active', '@if($is_active)  Yes @else <span class="text-red">No</span> @endif');
+
+        return $dt;
     }
 
 }
