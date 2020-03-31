26e2a232a5901af567aee8071c397a3173cc122c
Raihan Sikder <raihan.act@gmail.com>
2020-04-01 00:34:47 +0600

    #

--- app/Mainframe/Helpers/Cache.php
+++ app/Mainframe/Helpers/Cache.php
@@ -9,12 +9,12 @@ class Cache extends \Illuminate\Support\Facades\Cache
 
     /**
      *
-     * @param  string  $key
+     * @param  string $key
      * @return \Illuminate\Config\Repository|int|mixed
      */
     public static function time($key = 'none')
     {
-        if (config('mainframe.config.query-cache') == true && request('no_cache') != 'true') {
+        if (config('mainframe.config.query_cache') == true && request('no_cache') != 'true') {
             return config('mainframe.cache-time.'.$key);
         }
 
@@ -46,8 +46,8 @@ class Cache extends \Illuminate\Support\Facades\Cache
     /**
      * Caches a raw SQL query for given minutes.
      *
-     * @param  string  $sql  Raw SQL statement
-     * @param  int  $seconds  Minutes to cache
+     * @param  string $sql Raw SQL statement
+     * @param  int $seconds Minutes to cache
      * @return array|mixed Array of objects as query result
      */
     public static function rawQuery($sql, $seconds = 0)
