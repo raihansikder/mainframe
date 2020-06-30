f26b979f5cf286016fb09e2c96b40f165a1f0fd1
Raihan Sikder <raihan.act@gmail.com>
2020-06-30 22:24:53 +0600

    #

--- resources/views/mainframe/modules/settings/form/default.blade.php
+++ resources/views/mainframe/modules/settings/form/default.blade.php
@@ -22,15 +22,49 @@ $types = Setting::$types;
         @include('form.select-array',['var'=>['name'=>'type','label'=>'type', 'options'=>$types]])
         <div class="clearfix"></div>
 
-        @include('form.textarea',['var'=>['name'=>'value','label'=>'Value']])
+        {{--        @include('form.textarea',['var'=>['name'=>'value','label'=>'Value']])--}}
         <div class="clearfix"></div>
+
+        <div class="clearfix"></div>
+        <a class="btn btn-default pull-right" href="https://jsoneditoronline.org/#right=local.zelija&left=cloud.2c8cda9906ed4a7fa776b6674ab1bcda">
+            Use online JSON Formatter
+        </a>
+        <div class="clearfix"></div>
+
+        <div class="col-md-6 no-padding">
+
+            @include('form.textarea',['var'=>['name'=>'value','label'=>'Value(For array type put JSON)','div'=>'col-md-12']])
+            <div class="col-md-12 no-padding-l">
+                @dump($element->getValue())
+            </div>
+        </div>
+
+        <label class="control-label ">
+            Sample JSON for configuring invoice
+        </label>
+        <pre>
+{
+  "invoices": [
+    {
+      "event": "order.status.accepted",
+      "percentage": 50,
+      "due_after_days": 10
+    },
+    {
+      "event": "order.status.completed",
+      "percentage": 50,
+      "due_after_days": 30
+    }
+  ]
+}
+</pre>
 
         @include('form.textarea',['var'=>['name'=>'description','label'=>'Description', 'params'=>['class'=>'ckeditor']]])
         @include('form.is-active')
 
         {{--    Form inputs: ends    --}}
 
-            @include('form.action-buttons')
+        @include('form.action-buttons')
         {{ Form::close() }}
     </div>
 @endsection
