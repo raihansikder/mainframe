92920b6936cf13e7da079eb71f66b89212da17a2
Raihan Sikder <raihan.act@gmail.com>
2020-07-12 20:27:07 +0600

    #

--- app/Mainframe/Features/Modular/Skeleton/views/grid/default.blade.php
+++ app/Mainframe/Features/Modular/Skeleton/views/grid/default.blade.php
@@ -1,2 +1,2 @@
-@extends('projects.{project-name}.layouts.module.form.template')
+@extends('projects.{project-name}.layouts.module.grid.template')
 {{--@extends('mainframe.layouts.module.grid.template')--}}
