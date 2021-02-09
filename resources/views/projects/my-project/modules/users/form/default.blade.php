1629b4d2c659d357b90b7e4f6b50fa3a878ec160
Raihan Sikder <raihan.act@gmail.com>
2021-02-09 14:19:10 +0600

    #

--- resources/views/projects/my-project/modules/users/form/default.blade.php
+++ resources/views/projects/my-project/modules/users/form/default.blade.php
@@ -2,7 +2,7 @@
 
 <?php
 /**
- * @var \App\Projects\MphMarket\Modules\Users\User $element
+ * @var \App\User $element
  * @var string $formState create|edit
  * @var string $formState
  * @var array $formConfig
@@ -15,23 +15,32 @@
 @section('content')
     <div class="col-md-12 col-lg-10 no-padding">
 
-        @if(($formState === 'create'))
+        @if(($formState == 'create'))
             {{ Form::open($formConfig) }} <input name="uuid" type="hidden" value="{{$uuid}}"/>
-        @elseif($formState === 'edit')
+        @elseif($formState == 'edit')
             {{ Form::model($element, $formConfig)}}
         @endif
 
         {{--    Form inputs: starts    --}}
         {{--   --------------------    --}}
-        {{--email--}}
-        @include('form.text',['var'=>['name'=>'email','label'=>'Email(Username)','div'=>'col-md-4']])
-        {{--first_name--}}
-        @include('form.text',['var'=>['name'=>'first_name','label'=>'First Name']])
-        {{--last_name--}}
-        @include('form.text',['var'=>['name'=>'last_name','label'=>'Surname']])
+        @include('form.text',['var'=>['name'=>'email','label'=>'Email']])
 
         <div class="clearfix"></div>
+        @include('form.text',['var'=>['name'=>'first_name','label'=>'First Name']])
+        @include('form.text',['var'=>['name'=>'last_name','label'=>'Last Name']])
 
+        {{-- show password only for editable--}}
+        @if($editable)
+            <div class="clearfix"></div>
+            <h3>Password</h3>
+            @include('form.text',['var'=>['name'=>'password','type'=>'password','label'=>'New password','value'=>'']])
+            @include('form.text',['var'=>['name'=>'password_confirmation','type'=>'password','label'=>'Confirm new password']])
+        @endif
+
+        <div class="clearfix"></div>
+        @include('form.datetime',['var'=>['name'=>'email_verified_at','label'=>'Email verified at']])
+
+        <div class="clearfix"></div>
         <?php
         // myprint_r($element->group_ids);
         $var = [
@@ -43,104 +52,54 @@
             'params' => ['multiple', 'id' => 'groups'],
             'container_class' => 'col-sm-3'
         ];
-        //19 and 20 for vendor user and vendor admin
-        if (user()->ofVendor()) {
-            $var ['query'] = DB::table('groups')->whereIn('id', [19, 20]);
-        }
-            //21 and 22 for reseller user, reseller admin and client
-        if (user()->ofReseller()) {
-            $var ['query'] = DB::table('groups')->whereIn('id', [21, 22, 23]);
-        }
-            // 19,20,21,22,23,25
-            if (user()->isAdminL2()) {
-                $var ['query'] = DB::table('groups')->whereIn('id', [19, 20, 21, 22, 23, 25]);
-            }
-            // 19,20,21,22 28,29
-            if (user()->isSalesAdmin()) {
-                $var ['query'] = DB::table('groups')->whereIn('id', [19,20,21, 22,23, 28, 29]);
-            }
-            // 21,22,29
-            if (user()->isSalesMember()) {
-                $var ['query'] = DB::table('groups')->whereIn('id', [21, 22, 29]);
-            }
         ?>
         @include('form.select-model-multiple', compact('var'))
-        <?php
-        $var_vendor = [
-            'name' => 'vendor_id',
-            'label' => 'Vendor/Manufacturer',
-            'table' => 'vendors',
-            'show_inactive' => true
-        ];
-        if (user()->ofVendor()) {
-            $var_vendor['query'] = DB::table('vendors')->where('id', user()->vendor_id);
-        }
-        ?>
-        @if(user()->ofVendor())
-            @include('form.plain-text',['var'=>['label'=>'Vendor/Manufacturer','value'=>user()->vendor->name]])
-            @include('form.hidden',['var'=>['name'=>'vendor_id','value'=>user()->vendor_id]])
-        @else
-            <div class="opt_19">
-                @include('form.select-model',['var'=>$var_vendor])
+
+        <div class='clearfix'></div>
+        <div class="panel-group" id="accordion">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h5 class="panel-title">
+                        <a data-toggle="collapse" href="#other_info">
+                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Token
+                        </a>
+                    </h5>
+                </div>
+                <div id="other_info" class="panel-collapse collapse" style="margin:15px 0;">
+                    <div class="col-md-12">
+                        <div class="col-md-12 no-padding">
+                            {{--auth_token--}}
+                            @include('form.text',['var'=>['name'=>'api_token','label'=>'API token', 'container_class'=>'col-sm-8']])
+                            <br>
+                            <div class="control-group">
+                                <div class="controls">
+                                    <div class="form-group">
+                                        {{--@include('form.checkbox',['var'=>['name'=>'api_token_generate','label'=>'New token (Check and save)','value'=>"yes"]])--}}
+                                        <button id="api_token_generate" name="api_token_generate" class="btn btn-default">
+                                            <i class="fa fa-refresh"></i> Re-generate
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {{--auth_token--}}
+                        @include('form.plain-text',['var'=>['name'=>'auth_token','label'=>'Auth token', 'container_class'=>'col-sm-6']])
+                        {{--api_token_generated_at--}}
+                        @include('form.plain-text',['var'=>['name'=>'api_token_generated_at','label'=>'Api token generated at', 'container_class'=>'col-sm-6']])
+                        {{--device_name--}}
+                        @include('form.plain-text',['var'=>['name'=>'device_token','label'=>'Device token', 'container_class'=>'col-sm-12']])
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
             </div>
-        @endif
-        <?php
-        $var_reseller = [
-            'name' => 'reseller_id',
-            'label' => 'Partner',
-            'table' => 'resellers',
-            'show_inactive' => true,
-
-        ];
-        if (user()->ofReseller()) {
-            $var_reseller['query'] = DB::table('resellers')->where('id', user()->reseller_id);
-
-        }?>
-
-        <div class="opt_21">
-            @include('form.select-model',['var'=>$var_reseller])
         </div>
-
-        <?php
-        $var_client = [
-            'name' => 'client_id',
-            'label' => 'Client',
-            'table' => 'clients',
-            'show_inactive' => true,
-
-        ];
-        if (user()->ofReseller()) {
-            $var_client['query'] = DB::table('clients')->where('reseller_id', user()->reseller_id);
-
-        }?>
-        <div class="opt_23">
-            @include('form.select-model',['var'=>$var_client])
-        </div>
-        {{-- show password only for editable--}}
-        @if($editable)
-            <div class="clearfix"></div>
-            <h3>Password</h3>
-            @include('form.text',['var'=>['name'=>'password','type'=>'password','label'=>'New password','value'=>'']])
-            @include('form.text',['var'=>['name'=>'password_confirmation','type'=>'password','label'=>'Confirm new password']])
-        @endif
         <div class="clearfix"></div>
-        {{--reseller and vendor user should not see email verified at--}}
-            @if(!user()->inGroupIds(['19','20','22','23']))
-            @include('form.datetime',['var'=>['name'=>'email_verified_at','label'=>'Email verified at']])
-        @endif
-        <div class="clearfix"></div>
-        @include('projects.my-project.modules.users.form.includes.contact-fields')
 
-        <div class="clearfix"></div>
-        {{--token--}}
-        @if(user()->can('updateToken', $element) && (!user()->ofReseller() && !user()->ofVendor()))
-            @include('projects.my-project.modules.users.form.includes.token-fields')
-        @endif
-        @if(user()->isSuperUser())
-            @include('form.is-active')
-        @endif
+        @include('form.is-active')
         {{--    Form inputs: ends    --}}
+
         @include('form.action-buttons')
+
         {{ Form::close() }}
     </div>
 @endsection
@@ -157,4 +116,24 @@
 @section('js')
     @parent
     @include('projects.my-project.modules.users.form.js')
+    <script>
+        //Make the api_token readonly
+        $("input[name=api_token]").attr('readonly', true);
+
+        //JS based api_token re-generator
+        function randomString(len, charSet) {
+            charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var randomString = '';
+            for (var i = 0; i < len; i++) {
+                var randomPoz = Math.floor(Math.random() * charSet.length);
+                randomString += charSet.substring(randomPoz, randomPoz + 1);
+            }
+            return randomString;
+        }
+        $("#api_token_generate").click(function (e) {
+            event.preventDefault(e);
+            $("input[name=api_token]").val(randomString(64));
+            console.log(randomString(64));
+        });
+    </script>
 @endsection
