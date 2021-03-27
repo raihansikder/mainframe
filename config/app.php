9aad9b209a0603f04e7fa6039f96adb34efc3e98
Raihan Sikder <raihan.act@gmail.com>
2021-03-27 12:43:53 +0600

    #

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
         //Barryvdh\Debugbar\ServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Collective\Remote\RemoteServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -236,7 +238,9 @@ return [
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
+        // Packages
+        'SSH' => Collective\Remote\RemoteFacade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
         // Project
         'User' => App\User::class,
         'Upload' => \App\Mainframe\Modules\Uploads\Upload::class,
