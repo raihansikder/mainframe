5996cdfcf57e3d8190bdb2f98917ddc1a6fb478b
Raihan Sikder <raihan.act@gmail.com>
2021-03-28 20:15:54 +0600

    #

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,1 +1,11 @@
-<?php
+@servers(['localhost' => '127.0.0.1'])
+
+@task('deploy', ['on' => 'localhost'])
+
+
+@if ($branch)
+    git pull origin {{ $branch }}
+@endif
+
+php artisan migrate
+@endtask
