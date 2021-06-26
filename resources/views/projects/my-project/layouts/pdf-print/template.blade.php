f634b76d458586af1e4ddd3f8b67275ddc3e229c
Raihan Sikder <raihan.act@gmail.com>
2021-06-26 18:39:26 +0600

    #

--- resources/views/projects/my-project/layouts/pdf-print/template.blade.php
+++ resources/views/projects/my-project/layouts/pdf-print/template.blade.php
@@ -2,14 +2,11 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    @include('projects.vscript.layouts.default.includes.analytics')
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('projects/vscript/css/print-pdf.css')}}" type="text/css"/>
-    @section('head')
-
-    @show
+    @include('mainframe.layouts.default.includes.css')
+{{--    <link rel="stylesheet" href="{{asset('projects/vscript/css/print-pdf.css')}}" type="text/css"/>--}}
     <title>
         @section('head-title')
             {{setting('app-name')}}
@@ -21,6 +18,7 @@
 
     @section('css')
     @show
+    @include('projects.vscript.layouts.default.includes.analytics')
 </head>
 <body>
 <div class="container">
@@ -31,11 +29,10 @@
                 <tr>
                     <td width="50%" align="left" style="vertical-align: middle">
                         @section('title')
-                            <h2 style="padding-left: 10px" class="pull-left">{{config('app.name')}}</h2>
+                            <h1 class="pull-left">{{config('app.name')}}</h1>
                         @show
                         {{--<img class="image img-responsive"--}}
                         {{--src="{{asset('projects/vscript/images/logo.png')}}" alt=""/>--}}
-
                     </td>
                     <td width="50%" align="right" style="vertical-align: middle">
                         @section('title-right')
@@ -71,10 +68,9 @@
         <div class="col-md-12" id="footer">
             @section('footer')
             @show
-            <hr>
+            {{-- <hr>--}}
         </div>
     </div>
-
 </div>
 
 </body>
